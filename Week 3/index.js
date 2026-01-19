566abb8a41fc045170afcc2bb0439284e207ae3c
Anthony Burgic <144724632+AnthonyBurgic1@users.noreply.github.com>
2026-01-19 12:36:57 -0500

    Saved

--- Week 3/index.js
+++ Week 3/index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get("/", (req, res) => {
+    res.send("Hellow World");
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
