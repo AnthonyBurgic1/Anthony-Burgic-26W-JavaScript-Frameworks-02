ded7acf5b76ff4dceb15a4171ab5285eb666d78f
Anthony Burgic <144724632+AnthonyBurgic1@users.noreply.github.com>
2026-01-19 12:58:43 -0500

    save

--- Week 3/index.ts
+++ Week 3/index.ts
@@ -6,16 +6,16 @@ const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
+});
 
 app.get("/about", (req, res) => {
     res.send("This is the about page");
-})
+});
 
 app.get("/", (req, res) => {
     res.send("Hellow World");
-})
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
