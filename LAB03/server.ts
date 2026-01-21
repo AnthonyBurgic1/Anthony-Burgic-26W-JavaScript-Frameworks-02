4598692aa1703a40abe44118918966a78ddf5fc9
Anthony Burgic <144724632+AnthonyBurgic1@users.noreply.github.com>
2026-01-21 13:50:10 -0500

    So from line 13 - 72 i codded the following listed below:
     I have added all query parameters, i have added the validation for the x - y 
    
     i have added all operations for: switch (method) {
            case "add":
                result = x + y;
                operator = "+";
                break;
    
            case "subtract":
                result = x - y;
                operator = "-";
                break;
    
            case "multiply":
                result = x * y;
                operator = "*";
                break;
            
            case "divide":
                if (y === 0) {
                    res.send("Error: Cannot divide by zero");
                    return;
                }
                result = x / y;
                operator = "/";
                break;
    
    
    I have also added the invaild handler

--- LAB03/server.ts
+++ LAB03/server.ts
@@ -9,3 +9,64 @@ const app = express();
 
 // port number from the server will run on: 
 const port = 3000;
+
+// Here i have routed the calculator operations:
+// A good example is: http://localhost:3000/lab2?method=add&x=16&y=4 
+app.get("/lab3", (req: request, res: response) => {
+
+    // Here are the query parameters:
+    const method = req.query.method as string;
+    const x = Number(req.query.x);
+    const y = Number(req.query.y);
+
+    // The varaibles are stored in the result of the Math operator
+    let result: number;
+    let operator: string;
+
+    // This validates that x and y are numbers:
+    if (isNaN(x) || isNaN(y)) {
+        res.send("Error: x and y must be numbers.");
+        return;
+    }
+
+    // Here we can determine whcih math operation to perform:
+    switch (method) {
+        case "add":
+            result = x + y;
+            operator = "+";
+            break;
+
+        case "subtract":
+            result = x - y;
+            operator = "-";
+            break;
+
+        case "multiply":
+            result = x * y;
+            operator = "*";
+            break;
+        
+        case "divide":
+            if (y === 0) {
+                res.send("Error: Cannot divide by zero");
+                return;
+            }
+            result = x / y;
+            operator = "/";
+            break;
+
+        // This handle is invalid because of the method values:
+        default:
+            res.send("Error: Invalid method");
+            return;
+    }
+
+    // Here we are going to display the full Math Operation and the result:
+    res.send(`${x} ${operator} ${y} = ${result}`);
+
+});
+
+// And here are going to start the server and listen for the request we need:
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
