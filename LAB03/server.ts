1602752afc652c945a1f9a5cdcefadb53bbb5ff1
Anthony Burgic <144724632+AnthonyBurgic1@users.noreply.github.com>
2026-01-21 13:18:23 -0500

    From line 1 - 11 i have added: import from express
    i have added the request and the response from express.
    
    I have also added const app = express
    
    and i have added the port and made run at 3000

---
+++ LAB03/server.ts
@@ -0,0 +1,11 @@
+// here i have imported express frameworks:
+import express from "express";
+
+// Here is the request and response types of typescript:
+import { request, response } from "express";
+
+// Create an express application:
+const app = express();
+
+// port number from the server will run on: 
+const port = 3000;
